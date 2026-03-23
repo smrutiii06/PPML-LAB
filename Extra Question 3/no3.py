ec0658323b4aa643d69672f86e8455476e6798a3
smrutiii06 <smrutii2006@gmail.com>
2026-03-23 09:44:28 +0530

    Add files via upload

---
+++ Extra Question 3/no3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+zeros = np.zeros((2,3))
+ones = np.ones((2,3))
+empty = np.empty((2,3))
+
+print("Zeros array :\n", zeros)
+print("Ones array :\n", ones)
+print("Emptys array :\n", empty)
